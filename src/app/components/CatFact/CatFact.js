9940e90af8f2b4d4c60f3b86312044e83b20bfcb
sfriholm <sofiefriholm@gmail.com>
2025-06-08 21:43:32 +0200

    feat: add letter-by-letter animation to cat fact

--- src/app/components/CatFact/CatFact.js
+++ src/app/components/CatFact/CatFact.js
@@ -1,12 +1,14 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { getRandomCatFact } from '../../services/catFacts';
 import Button from '../Button/Button';
 import styles from './CatFact.module.css';
+import anime from 'animejs';
 
 export default function CatFact() {
     const [catFact, setCatFact] = useState("");
     const [loading, setLoading] = useState(false);
+    const factRef = useRef(null);
 
     async function fetchFact() {
         setLoading(true);
@@ -14,6 +16,26 @@ export default function CatFact() {
         setCatFact(newFact);
         setLoading(false);
     }
+
+    useEffect(() => {
+        if (!catFact || !factRef.current) return; // Exit if there is no cat fact or ref is not set
+
+        const element = factRef.current; // Get reference to the paragraph element where the fact will be displayed
+        
+        // Replace text with individual <span>-elements around each letter
+        // Allows to animate each letter separately
+        element.innerHTML = catFact.split("").map((letter) => `<span class="letter">${letter}</span>`).join("");
+
+        // Animate the letters with fade-in and slide-up effect
+        anime.timeline().add({
+            targets: ".letter", // Select all the <span class="letter">
+            opacity: [0, 1], // Fade in from transparent
+            translateY: [10, 0], // SLide up from 10px below
+            easing: "easeOutExpo",
+            duration: 100,
+            delay: anime.stagger(20), // Delay between letters
+        });
+    }, [catFact]); // Run this when the catFact changes
 
     return (
         <article className={styles.article}>
@@ -24,7 +46,7 @@ export default function CatFact() {
                 {catFact ? "Get another cat fact" : "Generate a cat fact"}
             </Button>
             {loading && <p>Loading...</p>}
-            {!loading && catFact && ( <p className={styles.fact}>{catFact}</p>)}
+            {!loading && catFact && ( <p ref={factRef} className={styles.fact}>{catFact}</p>)}
         </article>
-    )
+    );
 }
