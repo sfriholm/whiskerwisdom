fac4d977e09c99020d5987af2f78d98898257671
sfriholm <sofiefriholm@gmail.com>
2025-06-08 20:58:44 +0200

    feat: add click animation with scale and glow to button

--- src/app/components/Button/Button.js
+++ src/app/components/Button/Button.js
@@ -1,10 +1,24 @@
 'use client';
 import styles from './Button.module.css'
+import anime from 'animejs';
 
 export default function Button({ onClick, children, disabled }) {
+
+    function handleClick(e) {
+        anime({
+            targets: e.currentTarget,
+            scale: [1, 1.5], // Scale up 50%
+            boxShadow: ["0 0 0px transparent", "0 0 20px #9cbd8f"], // Green glow around the button on click
+            duration: 300,
+            easing: "easeInOutQuad",
+            direction: "alternate", // Goes back to original
+        });
+
+        if (onClick) onClick();
+    }
     return (
         <button 
-        onClick={onClick}
+        onClick={handleClick}
         disabled={disabled}
         type="button"
         className={styles.button}
